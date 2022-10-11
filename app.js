f53b4c254222bbab9b4fc2859b4bf7f7c40c4533
EhWanu <cameronjshadgett@gmail.com>
2022-10-11 13:55:01 -0400

    months layout and js boxes

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const boxContainer = document.querySelector(
+	".boxContainer"
+);
+
+// for loop creating 365 boxes
+for (let i = 0; i < 365; i++) {
+	const el = document.createElement("div");
+	el.classList = "box";
+	boxContainer.appendChild(el);
+}
